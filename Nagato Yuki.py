d6f38f012bb97346370d26bcb113c2551cc87705
PigeonTV2333 <97788070+PigeonTV2333@users.noreply.github.com>
2022-12-20 15:46:53 +0800

    Add files via upload

---
+++ Nagato Yuki.py
@@ -0,0 +1,35 @@
+import time
+import os
+os.system("CLS")
+raw_input()
+print "YUKI.N>これをあなたが読んでいる時、"
+print "       わたしはわたしではないだろう。"
+print ""
+time.sleep(3)
+print "YUKI.N>このメッセージが表示されたということは、"
+print "       そこにはあなた、わたし、涼宮ハルヒ、朝比奈みくる、"
+print "       古泉一樹が存在しているはずである。"
+print ""
+time.sleep(3)
+print "YUKI.N>それが鍵。"
+print "        あなたは解答を見つけ出した。"
+print ""
+time.sleep(3)
+print "YUKI.N>これは緊急脱出プログラムである。"
+print ""
+print "       起動させろ場合はエンターキーを、"
+print "       そうでない場合はそれ以外のキーを選択せよ。"
+print ""
+print "       起動させた場合、"
+print "       あなたは時空修正の機会を得る。"
+print "       ただし成功は保証できない。"
+print "       まだ帰還の保証もできない。"
+print ""
+time.sleep(5)
+print "YUKI.N>このプログラムが起動するのは一度きりである。"
+print "       実行ののち、消去される。"
+print ""
+print "       非実行が選択された場合は起動せずに消去される。"
+print ""
+time.sleep(3)
+raw_input("       Ｒｅａｄｙ？")
